a3ab69c017f74492e107607f415ffc138c366ab6
ovaraksin <ovaraksin@googlemail.com>
2011-05-02 20:40:42 +0000

    More implemented

--- src/main/java/com/googlecode/whiteboard/converter/PixelConverter.java
+++ src/main/java/com/googlecode/whiteboard/converter/PixelConverter.java
@@ -12,9 +12,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
-import javax.faces.convert.FacesConverter;
 
-@FacesConverter(value = "com.googlecode.whiteboard.converter.PixelConverter")
 public class PixelConverter implements Converter
 {
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
