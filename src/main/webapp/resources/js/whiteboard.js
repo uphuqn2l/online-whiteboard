06507fd4956b557236884ae091db8a09361f93f1
ovaraksin <ovaraksin@googlemail.com>
2011-05-04 10:23:01 +0000

    Initial implementation for whiteboard page and all panels inside

--- src/main/webapp/resources/js/whiteboard.js
+++ src/main/webapp/resources/js/whiteboard.js
@@ -1,12 +1,17 @@
 jQuery(function() {
     var parentT = jQuery('#toolboxDialog').parent();
-    parentT.css('left', parentT.offset().left - 10 + 'px');
+    parentT.css('left', parentT.offset().left - 325 + 'px');
 
     var parentP = jQuery('#propertiesDialog').parent();
     parentP.css('left', parentP.offset().left - 10 + 'px');
-    parentP.css('top', parentP.offset().top - 10 + 'px');
 
     var parentM = jQuery('#monitoringDialog').parent();
     parentM.css('top', parentM.offset().top - 10 + 'px');
+
+    var toolboxItems = jQuery('.toolboxItem');
+    toolboxItems.click(function() {
+        toolboxItems.removeClass('ui-state-selected');
+        jQuery(this).addClass('ui-state-selected');
+    });
 });
 
