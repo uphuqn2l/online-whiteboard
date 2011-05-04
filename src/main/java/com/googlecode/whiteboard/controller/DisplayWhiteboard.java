2e1caa1ee05bfba27e0b10e065087366793138b9
ovaraksin <ovaraksin@googlemail.com>
2011-05-04 21:49:59 +0000

    First code ready for deployment.

--- src/main/java/com/googlecode/whiteboard/controller/DisplayWhiteboard.java
+++ src/main/java/com/googlecode/whiteboard/controller/DisplayWhiteboard.java
@@ -6,10 +6,13 @@
 package com.googlecode.whiteboard.controller;
 
 import com.googlecode.whiteboard.errorhandler.DefaultExceptionHandler;
+import com.googlecode.whiteboard.model.AbstractElement;
 import com.googlecode.whiteboard.model.Whiteboard;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,6 +24,8 @@ public class DisplayWhiteboard implements Serializable
 
     private Whiteboard whiteboard;
     private WhiteboardsManager whiteboardsManager;
+    private String toolboxButtonId = "btnNo";
+    private AbstractElement selectedElement;
 
     @PostConstruct
     protected void initialize() {
@@ -68,5 +73,50 @@ public class DisplayWhiteboard implements Serializable
     public int getActiveUsers() {
         // TODO
         return 1;
+    }
+
+    public String getInvitationLink() {
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        String scheme = ec.getRequestScheme();
+        int port = ec.getRequestServerPort();
+
+        String serverPort;
+        if (("http".equalsIgnoreCase(scheme) && port != 80) || ("https".equalsIgnoreCase(scheme) && port != 443)) {
+            serverPort = ":" + port;
+        } else {
+            serverPort = "";
+        }
+
+        return ec.encodeResourceURL(scheme + "://" + ec.getRequestServerName() + serverPort + ec.getRequestContextPath() + "/views/whiteboard.jsf?uuid=" + whiteboard.getUuid());
+    }
+
+    public AbstractElement getSelectedElement() {
+        return selectedElement;
+    }
+
+    public void setSelectedElement(AbstractElement selectedElement) {
+        this.selectedElement = selectedElement;
+    }
+
+    public void selectToolboxItem(ActionEvent ae) {
+        toolboxButtonId = ae.getComponent().getId();
+
+        if (toolboxButtonId.equals("btnClone")) {
+            // clone selected element if any exists and show it in edit or show 'no element selected'
+            // TODO
+        } else if (toolboxButtonId.equals("btnRemove")) {
+            // remove selected element and show 'no selection' if any exists or 'no element selected'
+            // TODO
+        } else if (toolboxButtonId.equals("btnClearWb")) {
+            // clear whiteboard and show 'no selection'
+            // TODO
+        } else if (toolboxButtonId.equals("btnResizeWb")) {
+            // resize whiteboard
+            // TODO
+        }
+    }
+
+    public boolean isToolboxItemSelected(String itemId) {
+        return toolboxButtonId.equals(itemId);
     }
 }
